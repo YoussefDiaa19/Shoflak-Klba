b0e6888182cc2c53e1c6357f0942b8113f762a25
Youssef Diaa <youssefdiaa19@gmail.com>
2026-06-05 15:30:58 +0300

    chore: downgrade @capacitor-community/fcm to 6.0.1
    Downgrade dependency to ensure compatibility with Capacitor 6.

---
+++ ios/App/App/public/assets/index-legacy-DksG7mQs.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-D3KSySCN.js"],function(e,t){"use strict";var n,r;return{setters:[function(e){n=e.r,r=e._}],execute:function(){e("FCM",n("FCM",{web:function(){return r(function(){return t.import("./web-legacy-B426pBW7.js")},void 0,t.meta.url).then(function(e){return new e.FCMWeb})}}))}}});
